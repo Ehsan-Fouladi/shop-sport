6a536e98f4d50d9da1f2dc8a31737eed96f4ebbc
Ehsan-Fouladi <fooehan3@gmail.com>
2026-08-20 23:54:15 +0330

    feature: install and config shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fa" dir="rtl" translate="no" className={`h-full antialiased`}>
+    <html lang="fa" dir="rtl" translate="no" className={cn("h-full", "antialiased", "font-sans")}>
       <body className="w-full h-full">{children}</body>
     </html>
   );
