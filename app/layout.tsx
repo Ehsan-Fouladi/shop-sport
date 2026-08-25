f2bc097bc8de26e1778ec09514d0ff1066b0b68a
Ehsan-Fouladi <fooehan3@gmail.com>
2026-08-26 01:00:35 +0330

    fix: load font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fa" dir="rtl" translate="no" className={cn("h-full", "antialiased", "font-sans")}>
+    <html lang="fa" dir="rtl" translate="no">
       <body className="w-full h-full">{children}</body>
     </html>
   );
