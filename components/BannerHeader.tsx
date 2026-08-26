b14c0a73d7259a29768dee8705268153ebdd9734
Ehsan-Fouladi <fooehan3@gmail.com>
2026-08-27 00:53:04 +0330

    added new component ui and make header and footer and banner header

---
+++ components/BannerHeader.tsx
@@ -0,0 +1,9 @@
+export default function BannerHeader() {
+  return (
+    <div className="w-full h-10 bg-base-secondary flex justify-center items-center">
+      <span className="text-base-neutral text-sm font-bold">
+        با عضویت در فیت لند اولین ارسال را مهمون ما باش :)
+      </span>
+    </div>
+  );
+}
