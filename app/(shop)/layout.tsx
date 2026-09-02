8e3309c1c01f70ba1c2ea5e252e50c97036ec7e1
Ehsan-Fouladi <fooehan3@gmail.com>
2026-09-03 01:59:50 +0330

    make drawer menu mobile and resposive to mobile

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -1,4 +1,5 @@
 import BannerHeader from "@/components/header/BannerHeader";
+import DockMenu from "@/components/header/DockMenu";
 import Header from "@/components/header/Header";
 import Footer from "@/components/TheFooter";
 import { PropsWithChildren } from "react";
@@ -10,6 +11,7 @@ export default function ShopLayout({ children }: PropsWithChildren) {
       <Header />
       <main>{children}</main>
       <Footer />
+      <DockMenu/>
     </>
   );
 }
---
+++ data/menu.ts
@@ -0,0 +1,710 @@
+import { MenuItem } from "@/types/menu.types";
+
+export const menuItems: MenuItem[] = [
+  {
+    id: "man",
+    title: "مردانه",
+    children: [
+      {
+        id: "man-sportswear",
+        title: "لباس ورزشی مردانه",
+        children: [
+          {
+            id: "sportswear",
+            title: "ست لباس ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-dress",
+            title: "تیشرت ,بلوز و استرچ ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-competition",
+            title: "رکابی ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-kit",
+            title: "کیت ورزشی",
+            href: "",
+          },
+          {
+            id: "sweatshirt",
+            title: "سویشرت",
+            href: "",
+          },
+          {
+            id: "hoodie",
+            title: "هودی",
+            href: "",
+          },
+          {
+            id: "mayo",
+            title: "مایو",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "man-sports-pants",
+        title: "شلوار ورزشی مردانه",
+        children: [
+          {
+            id: "sports-pant",
+            title: "شلوار ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-pants",
+            title: "شلوارک ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-lag",
+            title: "لگ و نیم لگ",
+            href: "",
+          },
+          {
+            id: "sports-jagger",
+            title: "جاگر",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "man-accessory",
+        title: "اکسسوری",
+        children: [
+          {
+            id: "sports-socks",
+            title: "جوراب ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-helmet",
+            title: "کلاه ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-bracelet",
+            title: "دستبند ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-headbands",
+            title: "هدبند",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "man-sneakers",
+        title: "کفش ورزشی",
+        children: [
+          {
+            id: "climbing-shoes",
+            title: "کفش کوهنوردی",
+            href: "",
+          },
+          {
+            id: "skate-shoes",
+            title: "کفش اسکیت",
+            href: "",
+          },
+          {
+            id: "basketball-shoes",
+            title: "کفش بسکتبال",
+            href: "",
+          },
+          {
+            id: "ship-shoes",
+            title: "کفش کشتی",
+            href: "",
+          },
+          {
+            id: "running-linen",
+            title: "کتانی رانینگ",
+            href: "",
+          },
+          {
+            id: "stoke",
+            title: "استوک",
+            href: "",
+          },
+          {
+            id: "eembaei",
+            title: "دمبایی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "brand-unprecedented",
+        title: "برند بیشنهادی",
+        children: [
+          {
+            id: "nike",
+            title: "nike",
+            href: "",
+          },
+          {
+            id: "north-face",
+            title: "the north face",
+            href: "",
+          },
+          {
+            id: "under-armour",
+            title: "under armour",
+            href: "",
+          },
+          {
+            id: "adidas",
+            title: "adidas",
+            href: "",
+          },
+          {
+            id: "fila",
+            title: "fila",
+            href: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "woman",
+    title: "زنانه",
+    children: [
+      {
+        id: "woman-sportswear",
+        title: "لباس ورزشی زنانه",
+        children: [
+          {
+            id: "sportswear",
+            title: "ست لباس ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-t-shirts-blouses-stretch",
+            title: "تیشرت ,بلوز و استرچ ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-bust",
+            title: "نیم تنه ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-tab",
+            title: "تاب ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-kit",
+            title: "کیت ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-sweatshirt",
+            title: "سویشرت",
+            href: "",
+          },
+          {
+            id: "sports-hoodie",
+            title: "هودی",
+            href: "",
+          },
+          {
+            id: "sports-mayo",
+            title: "مایو",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "woman-sports-pants",
+        title: "شلوار ورزشی زنانه",
+        children: [
+          {
+            id: "sports-shalwar-exercise",
+            title: "شلوار ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-shallowark-workout",
+            title: "شلوارک ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-lag",
+            title: "لگ و نیم لگ",
+            href: "",
+          },
+          {
+            id: "sports-jagger",
+            title: "جاگر",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "woman-accessory",
+        title: "اکسسوری",
+        children: [
+          {
+            id: "sports-socks",
+            title: "جوراب ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-hat",
+            title: "کلاه ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-bracelet",
+            title: "دستبند ورزشی",
+            href: "",
+          },
+          {
+            id: "headband",
+            title: "هدبند",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "woman-sneakers",
+        title: "کفش ورزشی",
+        children: [
+          {
+            id: "climbing-shoes",
+            title: "کفش کوهنوردی",
+            href: "",
+          },
+          {
+            id: "climbing-shoes",
+            title: "کفش اسکیت",
+            href: "",
+          },
+          {
+            id: "basketball-shoes",
+            title: "کفش بسکتبال",
+            href: "",
+          },
+          {
+            id: "boat-shoes",
+            title: "کفش کشتی",
+            href: "",
+          },
+          {
+            id: "running-linen",
+            title: "کتانی رانینگ",
+            href: "",
+          },
+          {
+            id: "stoke",
+            title: "استوک",
+            href: "",
+          },
+          {
+            id: "dembaei",
+            title: "دمبایی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "woman-sports-socks",
+        title: "جوراب ورزشی",
+        children: [{ id: "sports-socks", title: "جوراب ورزشی", href: "" }],
+      },
+      {
+        id: "brand-unprecedented",
+        title: "برند بیشنهادی",
+        children: [
+          {
+            id: "nike",
+            title: "nike",
+            href: "",
+          },
+          {
+            id: "north-face",
+            title: "the north face",
+            href: "",
+          },
+          {
+            id: "under-armour",
+            title: "under armour",
+            href: "",
+          },
+          {
+            id: "adidas",
+            title: "adidas",
+            href: "",
+          },
+          {
+            id: "fila",
+            title: "fila",
+            href: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "children",
+    title: "بچگانه",
+    children: [
+      {
+        id: "sportswear-girls",
+        title: "لباس ورزشی دخترانه",
+        children: [
+          {
+            id: "sports-sportswear",
+            title: "ست لباس ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-t-shirts-blouses-stretch",
+            title: "تیشرت ,بلوز و استرچ ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-kit",
+            title: "کیت ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-sweatshirt",
+            title: "سویشرت",
+            href: "",
+          },
+          {
+            id: "sports-hoodie",
+            title: "هودی",
+            href: "",
+          },
+          {
+            id: "sports-mayo",
+            title: "مایو",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "girls-trousers",
+        title: "شلوار ورزشی دخترانه",
+        children: [
+          {
+            id: "sports-pant",
+            title: "شلوار ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-pants",
+            title: "شلوارک ورزشی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "boy-sportswear",
+        title: "لباس ورزشی پسرونه",
+        children: [
+          {
+            id: "sports-sportswear",
+            title: "ست لباس ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-t-shirts-blouses-stretch",
+            title: "تیشرت ,بلوز و استرچ ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-kit",
+            title: "کیت ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-sweatshirt",
+            title: "سویشرت",
+            href: "",
+          },
+          {
+            id: "sports-hoodie",
+            title: "هودی",
+            href: "",
+          },
+          {
+            id: "sports-mayo",
+            title: "مایو",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "boy-sweatpants",
+        title: "شلوار ورزشی پسرونه",
+        children: [
+          {
+            id: "sports-pant",
+            title: "شلوار ورزشی",
+            href: "",
+          },
+          {
+            id: "sports-pants",
+            title: "شلوارک ورزشی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "girls-sneakers",
+        title: "کفش ورزشی دخترانه",
+        children: [
+          {
+            id: "climbing-shoes",
+            title: "کفش کوهنوردی",
+            href: "",
+          },
+          {
+            id: "skate-shoes",
+            title: "کفش اسکیت",
+            href: "",
+          },
+          {
+            id: "basketball-shoes",
+            title: "کفش بسکتبال",
+            href: "",
+          },
+          {
+            id: "stock",
+            title: "استوک",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "boy-sneakers",
+        title: "کفش ورزشی پسرانه",
+        children: [
+          {
+            id: "climbing-shoes",
+            title: "کفش کوهنوردی",
+            href: "",
+          },
+          {
+            id: "skate-shoes",
+            title: "کفش اسکیت",
+            href: "",
+          },
+          {
+            id: "basketball-shoes",
+            title: "کفش بسکتبال",
+            href: "",
+          },
+          {
+            id: "stock",
+            title: "استوک",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "sports-socks",
+        title: "جوراب ورزشی",
+        children: [
+          {
+            id: "sports-socks",
+            title: "جوراب ورزشی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "brand-unprecedented",
+        title: "برند بیشنهادی",
+        children: [
+          {
+            id: "nike",
+            title: "nike",
+            href: "",
+          },
+          {
+            id: "north-face",
+            title: "the north face",
+            href: "",
+          },
+          {
+            id: "under-armour",
+            title: "under armour",
+            href: "",
+          },
+          {
+            id: "adidas",
+            title: "adidas",
+            href: "",
+          },
+          {
+            id: "fila",
+            title: "fila",
+            href: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "equipment",
+    title: "لوازم ورزشی",
+    children: [
+      {
+        id: "bodybuilding-equipment",
+        title: "لوازم بدنسازی",
+        children: [
+          {
+            id: "weights-dumbbells",
+            title: "وزنه و دمبل",
+            href: "",
+          },
+          {
+            id: "yoga-bilats",
+            title: "لوازم یوگا و بیلاتس",
+            href: "",
+          },
+          {
+            id: "fitness-bodybuilding",
+            title: "لوازم فیتنس و بدنسازی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "accessories",
+        title: "لوازم جانبی",
+        children: [
+          {
+            id: "wristband-legband-kneeband",
+            title: "مچ بند ,ساق بند ,زانو بند",
+            href: "",
+          },
+          {
+            id: "sports-bag",
+            title: "ساک ورزشی",
+            href: "",
+          },
+          {
+            id: "swimming-equipment",
+            title: "وسایل شنا",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "backpack-supplies",
+        title: "لوازم کوله پشتی",
+        children: [
+          {
+            id: "bag-shozbeg",
+            title: "کوله و شوزبگ",
+            href: "",
+          },
+          {
+            id: "other-supplies",
+            title: "لوازم دیگر",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "brand-unprecedented",
+        title: "برند بیشنهادی",
+        children: [
+          {
+            id: "nike",
+            title: "nike",
+            href: "",
+          },
+          {
+            id: "north-face",
+            title: "the north face",
+            href: "",
+          },
+          {
+            id: "under-armour",
+            title: "under armour",
+            href: "",
+          },
+          {
+            id: "adidas",
+            title: "adidas",
+            href: "",
+          },
+          {
+            id: "fila",
+            title: "fila",
+            href: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "accessories",
+    title: "شیکر و جاگ",
+    children: [
+      {
+        id: "shaker",
+        title: "شیکر",
+        children: [
+          {
+            id: "bodybuilding-shaker",
+            title: "شیکر بدنسازی",
+            href: "",
+          },
+          {
+            id: "blender-shakers-electric",
+            title: "مخلوط کن و شیکر برقی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "mag-flask",
+        title: "ماگ و فلاسک",
+        children: [
+          {
+            id: "mag-flask",
+            title: "ماگ و فلاسک",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "pill-pill",
+        title: "جا بودری و جا قرصی",
+        children: [
+          {
+            id: "pill-pill",
+            title: "جا بودری و جا قرصی",
+            href: "",
+          },
+        ],
+      },
+      {
+        id: "sports-thermos",
+        title: "قمقمه ورزشی",
+        children: [
+          {
+            id: "sports-thermos",
+            title: "قمقمه ورزشی",
+            href: "",
+          },
+        ],
+      },
+    ],
+  },
+];
